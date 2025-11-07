4e963eba7b59d981bb62cbd2c7f0b202d4dd7fb8
stefanoclemente <stefano.clemente@unimi.it>
2025-11-07 19:36:04 +0100

    Update degree_computation.py

--- degree_computation.py
+++ degree_computation.py
@@ -1,26 +1,38 @@
 import statistics
 
-file_path=r'C:\Users\scapp\OneDrive\Documenti\hypergraphs\arxiv_cats_sample.txt'
+file_path=r'C:\Users\scapp\OneDrive\Documenti\hypergraphs\hyperedges-wikipedia-pages.txt'
+#hyperedges-amazon-reviews.txt
+#hyperedges-stackoverflow-answers
+#hyperedges-google
+#hyperedges-wikipedia-pages
+#hyperedges-mathoverflow-answers
 def calcola_gradi(file_path, deg=5):
     # dizionario: vertice -> grado (numero di iperlati che lo contengono)
     gradi = {}
     gradi_grandi = []
     count = 0
     rank = 0
+    dimensione = 0
+    dimensione_gaifman = 0
     with open(file_path, "r") as f:
         for linea in f:
             linea = linea.strip()
             if not linea:
                 continue
             
+            
             vertici = set(int(x) for x in linea.split(",") if x.strip() != "")
+            dimensione+=len(vertici)
+            dimensione_gaifman+=(len(vertici)**2)
             count +=1
             if rank < len(vertici):
                 rank= len(vertici)
             for v in vertici:
                 gradi[v] = gradi.get(v, 0) + 1
-                if gradi[v] > deg:
-                    gradi_grandi.append((v, gradi[v]))
+            
+        for v in gradi.keys():
+            if gradi[v] > deg:
+                gradi_grandi.append((v, gradi[v]))
 
     if not gradi:
         raise ValueError("Il file non contiene iperlati / vertici.")
@@ -36,11 +48,11 @@ def calcola_gradi(file_path, deg=5):
 
     grado_mediano = statistics.median(lista_gradi)
 
-    return grado_massimo, grado_medio, gradi, gradi_grandi, grado_mediano, count, rank
+    return grado_massimo, grado_medio, gradi, gradi_grandi, grado_mediano, count, rank, dimensione, dimensione_gaifman
 
 
 if __name__ == "__main__":
-    grado_max, grado_med, gradi, gradi_grandi, grado_mediano, count, rank = calcola_gradi(file_path)
+    grado_max, grado_med, gradi, gradi_grandi, grado_mediano, count, rank, dimensione, dimensione_gaifman = calcola_gradi(file_path)
     print(f"Grado massimo: {grado_max}")
     print(f"Grado medio:   {grado_med}")
     print(f"Grado mediano:   {grado_mediano}")
@@ -49,3 +61,5 @@ if __name__ == "__main__":
     print(f"n° vertici {len(gradi)}")
     print(f"n° iperlati {count}")
     print(f"Rank: {rank}")
+    print(f"Dimensione:   {dimensione}")
+    print(f"Dimensione Gaifman:   {dimensione_gaifman}")
